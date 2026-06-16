216f278933ad9948ff57c8a4ec2e9204956c6700
icodexies <nrenuell@gmail.com>
2026-06-16 16:57:47 +0800

    logo fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="{{ Vite::asset('public/images/icodexies-logo.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/icodexies-logo.png') }}">
         @routes
         
         @vite(['resources/js/app.js', 'resources/css/app.css'])
