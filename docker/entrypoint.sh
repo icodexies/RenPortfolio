f9428705b3926cb3dac36568a9a103ad058a47f1
icodexies <nrenuell@gmail.com>
2026-06-16 16:48:48 +0800

    500 error fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,10 @@ set -e
 cd /var/www/html
 
 echo "→ Ensuring storage directories exist..."
-mkdir -p storage/framework/sessions storage/framework/views storage/framework/cache storage/framework/testing storage/logs bootstrap/cache
-chown -R www-data:www-data storage bootstrap/cache
+mkdir -p storage/framework/sessions storage/framework/views storage/framework/cache storage/framework/testing storage/logs bootstrap/cache database
+touch database/database.sqlite
+chown -R www-data:www-data storage bootstrap/cache database
+chmod -R 775 database
 
 echo "→ Caching config..."
 php artisan config:cache
